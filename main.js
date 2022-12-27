8f9b1e372f367c90cb4edec9337e45ef219f2a57
Gaurav Kapoor <gaurav.kapoor27@outlook.com>
2022-12-28 10:47:40 +1300

    added comments

--- main.js
+++ main.js
@@ -1,53 +1,58 @@
+// access main containers
 mainBox = document.querySelector(".container");
 historyBox = document.querySelector(".history-box");
+
+// prevent right click on main box
+mainBox.addEventListener("contextmenu", (e) => e.preventDefault());
+
+// create mean button
 meanButton = document.createElement("button");
 meanButton.classList.add("mean-btn");
 meanButton.style.left = "-5000px";
 historyBox.appendChild(meanButton);
 
+// instantiate variables
 let average;
 let scoresArray = [];
-let clickedEarly = false;
 let startTime;
 let reactionTime;
 let colorTimeout;
 
+// create start game function
 let startGame = () => {
   let timeDelay = (Math.random() * 3 + 2) * 1000;
-  // let timeDelay = 2000;
   mainBox.innerText = "";
   mainBox.style.backgroundColor = "#480000";
-  clickedEarly = false;
 
   clearTimeout(colorTimeout);
   colorTimeout = setTimeout(colorShow, timeDelay);
 
+  mainBox.removeEventListener("mousedown", startGame);
   mainBox.addEventListener("mousedown", earlyClick);
 };
 
+// create early click function
 let earlyClick = () => {
   mainBox.innerText = "You clicked too early!\nClick this box to start again.";
   mainBox.style.backgroundColor = "#480000";
-  clickedEarly = true;
   clearTimeout(colorTimeout);
   mainBox.removeEventListener("mousedown", earlyClick);
+  mainBox.addEventListener("mousedown", startGame);
 };
 
-mainBox.addEventListener("mousedown", startGame);
-
+// create color show function
 let colorShow = () => {
-  if (!clickedEarly) {
-    startTime = new Date();
-    mainBox.style.backgroundColor = "#066E00";
-    mainBox.removeEventListener("mousedown", earlyClick);
-    mainBox.removeEventListener("mousedown", startGame);
-    mainBox.addEventListener("mousedown", reactionClick);
-  }
+  startTime = new Date();
+  mainBox.style.backgroundColor = "#066E00";
+  mainBox.removeEventListener("mousedown", earlyClick);
+  mainBox.addEventListener("mousedown", reactionClick);
 };
 
+// create reaction click function
 let reactionClick = () => {
   reactionTime = new Date() - startTime;
   mainBox.innerText = `Your reaction time was ${reactionTime} ms.\nClick this box to start again.`;
+
   if (reactionTime >= 150 && reactionTime <= 350) {
     newScore = document.createElement("button");
     newScore.classList.add("btn");
@@ -66,7 +71,10 @@ let reactionClick = () => {
     meanButton.style.transform = `translateX(-50%)`;
     meanButton.innerText = `${Math.round(average)}`;
   }
+
   mainBox.removeEventListener("mousedown", reactionClick);
-  mainBox.removeEventListener("mousedown", earlyClick);
   mainBox.addEventListener("mousedown", startGame);
 };
+
+// start game
+mainBox.addEventListener("mousedown", startGame);
